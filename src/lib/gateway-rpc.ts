c7954f099c60fcc2624a23819d85b1258222410c
Tuan Tran <tuantranf@gmail.com>
2026-03-28 08:46:35 +0700

    feat: increase timeout

--- src/lib/gateway-rpc.ts
+++ src/lib/gateway-rpc.ts
@@ -27,7 +27,7 @@ const GATEWAY_OPERATOR_SCOPES = [
   'operator.pairing',
 ];
 
-const DEFAULT_TIMEOUT_MS = 10_000;
+const DEFAULT_TIMEOUT_MS = 180_000;
 
 // ---------------------------------------------------------------------------
 // Types
